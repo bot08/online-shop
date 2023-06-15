463f589988dc700aad7baf557b59b51336f25475
Oleg <scherbaoleg4@gmail.com>
2023-06-15 21:41:32 +0300

    fix SPA

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,16 @@ import '../styles/styles.css'
 import { ThemeProvider } from "next-themes";
 import store from '../store'
 import { Provider } from 'react-redux'
+import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <ThemeProvider forcedTheme={Component.theme || undefined} defaultTheme="white" enableSystem={false}  attribute="class">
-        <Component {...pageProps} />
+        <Navbar/>
+        <main className="container px-2 sm:px-4 md:px-8 lg:px-12 pt-16 mx-auto">
+          <Component {...pageProps} />
+        </main>
       </ThemeProvider>
     </Provider>
   )
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Navbar from '../components/Navbar'
 
 class MyDocument extends Document {
   render() {
@@ -9,11 +8,8 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <body className="min-h-screen bg-gray-100 dark:bg-gray-800 transition-colors">
-          <Navbar/>
-          <main className="container px-2 sm:px-4 md:px-8 lg:px-12 pt-16 mx-auto">
-            <Main/>
-            <NextScript />
-          </main>
+          <Main/>
+          <NextScript/>
         </body>
       </Html>
     )
