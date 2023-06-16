8d4b2831e05baae86d170b227b455ed86cb21d05
Oleg <scherbaoleg4@gmail.com>
2023-06-16 14:15:52 +0300

    add back to home btn

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 const ContactPage = () => {
   return (
@@ -7,6 +8,9 @@ const ContactPage = () => {
         <title>Contact - Online shop</title>
       </Head>
       <h2 className="mt-1 text-3xl text-gray-900 font-bold">TODO: Contact page</h2>
+      <Link href='/' className="px-4 py-1 bg-green-600 text-white text-normal rounded-xl shadow">
+        Back to home
+      </Link>
     </>
   )
 }
