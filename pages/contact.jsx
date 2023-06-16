88c17c6dea49dcdb12352dd6089f850c63990b33
Oleg <scherbaoleg4@gmail.com>
2023-06-16 13:54:57 +0300

    add contact todo

---
+++ pages/contact.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const ContactPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact - Online shop</title>
+      </Head>
+      <h2 className="mt-1 text-3xl text-gray-900 font-bold">TODO: Contact page</h2>
+    </>
+  )
+}
+
+export default ContactPage
