0cc35e2bb279540acc0ebdefccce8a852a626d70
Oleg <scherbaoleg4@gmail.com>
2023-06-26 21:51:05 +0300

    fix mobile

--- components/ProductId/Loader.jsx
+++ components/ProductId/Loader.jsx
@@ -2,9 +2,9 @@ const ProductsLoader = () => {
   return (
     <div className='grid md:grid-cols-2'>
       {/* Image */}
-      <div className="className='h-60 lg:h-96 m-4"></div>
+      <div className="h-60 lg:h-96 m-4"></div>
       {/* block */}
-      <div className=' m-4 p-3 bg-white shadow rounded-xl'></div>
+      <div className='m-4 p-3 h-80 bg-white shadow rounded-xl'></div>
     </div>
   )
 }
